332fc076256ba44a0180ce445e34b92e98c76a59
yurigol <yuri_gol@mail.ru>
2016-11-27 16:13:21 +0300

    удалили таблицу

--- SocialWeb/Models/Message.cs
+++ SocialWeb/Models/Message.cs
@@ -9,6 +9,8 @@ namespace SocialWeb.Models
     public class Message
     {
         public int Id { get; set; }
+        public int FromUserId { get; set; }
+        public int ToUserId { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
